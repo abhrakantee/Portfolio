5aef0577c9a6a2400b90664d185b83363f81a7c5
Abhrakantee Sarkar <abhraks007@gmail.com>
2026-06-15 00:45:50 +0530

    Updated custom cursor animation and smooth movement

--- script.js
+++ script.js
@@ -20,34 +20,3 @@ window.addEventListener("scroll", () => {
     }
   });
 });
-const cursor = document.querySelector('.cursor');
-
-let mouseX = 0, mouseY = 0;
-let cursorX = 0, cursorY = 0;
-
-document.addEventListener('mousemove', e => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-});
-
-// Smooth animation loop
-function animate() {
-  // Lerp (move a fraction closer each frame)
-  cursorX += (mouseX - cursorX) * 0.15;
-  cursorY += (mouseY - cursorY) * 0.15;
-
-  cursor.style.left = cursorX + 'px';
-  cursor.style.top = cursorY + 'px';
-
-  requestAnimationFrame(animate);
-}
-animate();
-
-// Click animation
-document.addEventListener('mousedown', () => {
-  cursor.style.transform = 'translate(-50%, -50%) scale(0.8)';
-});
-
-document.addEventListener('mouseup', () => {
-  cursor.style.transform = 'translate(-50%, -50%) scale(1)';
-});
